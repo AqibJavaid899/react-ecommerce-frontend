a9017a35307a512fb0c08e08e8216bd805f78ca4
Aqib Javaid <aqibjavaid@Tintashs-MacBook-Pro.local>
2022-11-21 13:01:08 +0500

    Setup the MUI Theme Setup and also structurize the application folder architecture with routing logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import { Home } from "./scenes/home";
+import { Checkout, Confirmation } from "./scenes/checkout";
+import { Navbar } from "./scenes/global";
+import { ItemDetails } from "./scenes/itemDetails";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+};
+
 function App() {
-  return <div className="app">Welcome to React!</div>;
+  return (
+    <div className="app">
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
 import App from "./App";
+import { theme } from "./theme";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
